ef074b955cad3d50410e5b08aa2397eab6b6b8cd
david postigo <david.postigo@estudiant.enti.cat>
2026-03-16 13:27:16 +0100

    movement changes

--- Assets/Scripts/player/grounddetected.cs
+++ Assets/Scripts/player/grounddetected.cs
@@ -1,11 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class grounddetected : MonoBehaviour
 {
     public bool isgrounded;
     public float distance;
+    public LayerMask layerMask;
+    public Vector3 offset;
+    public float radius;
     void Update()
     {
-        Debug.DrawRay(transform.position, Vector3.down, Color.red);
+        Debug.DrawRay(transform.position + offset, Vector3.down * distance, Color.red);
+
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position + offset, Vector2.down, distance, layerMask);
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position + offset, radius, Vector2.down, distance, layerMask);
+
+        if (hit.collider == null )
+        {
+            isgrounded = false;
+        }
+        else
+        {
+            isgrounded = true;
+            Debug.DrawRay(transform.position + offset, Vector3.down * hit.distance, Color.green);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = isgrounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(transform.position + offset, radius);
+        Gizmos.DrawWireSphere(transform.position + Vector3.down * distance, radius);
     }
 }
--- Assets/Scripts/player/jump.cs
+++ Assets/Scripts/player/jump.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class jump : MonoBehaviour
 {
+    grounddetected ground;
     Rigidbody2D rb;
     private InputSystem_Actions test;
     public int force;
     void Start()
     {
+        ground = GetComponent<grounddetected>();
         rb = GetComponent<Rigidbody2D>();
         test = new InputSystem_Actions();
         test.Enable();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(test.Player.Jump.WasPressedThisFrame())
+        if(test.Player.Jump.WasPressedThisFrame() && ground.isgrounded)
         {
             rb.AddForce(Vector2.up * force);
         }
